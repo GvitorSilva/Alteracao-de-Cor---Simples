2b2b918887c8f6624506ed633f9d31b28eb8ac34
Gabriel Vitor da Silva <83681897+GvitorSilva@users.noreply.github.com>
2021-11-12 11:59:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function mudaCor(cor){
+    var elemento = document.getElementById("mensagem")
+    elemento.style.color = cor;
+}
+
+/* elemento.style.backgroundColor = cor;
+elemento.style.fontSIze = 50px; */
+
